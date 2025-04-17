a03457a0640ed37138287ab2f405b2fa486f0921
Raghav Goel <raghavgoel168@gmail.com>
2025-04-17 16:23:23 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from Chest_Cancer_Classifier import logger
-# from src.Chest_Cancer_Classification import logger
+from Chest_Cancer_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Raghav")
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
