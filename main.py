41dc2feb8c4a59a75e5c99044f4c8bfda87d3bee
Raghav Goel <raghavgoel168@gmail.com>
2025-04-26 17:24:41 +0530

    dvc added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Chest_Cancer_Classifier import logger
 from Chest_Cancer_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Chest_Cancer_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from Chest_Cancer_Classifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+from Chest_Cancer_Classifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -34,6 +35,19 @@ try:
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
